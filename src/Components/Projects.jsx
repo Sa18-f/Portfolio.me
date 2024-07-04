0360c04b290bb4b2c001290a2d4885b218bf82e3
Sa18-f <sisazid182713@gmail.com>
2024-07-04 12:47:46 +0600

    skills section design touched

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -25,7 +25,7 @@ const Projects = () => {
     ];
     return (
         <section id="projects" className="container p-5">
-            <h2 className="text-gray-950 dark:text-gray-100 text-3xl md:text-5xl font-bold mb-2 tracking-tight text-start">My Featured Projects</h2>
+            <h2 className="text-gray-950 dark:text-gray-100 text-3xl md:text-5xl font-bold mb-2 tracking-tight text-start">My Featured Projects ⬇️</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {projects.map((project, index) => (
                     <div key={index} className="mt-8 shadow-md dark:bg-zinc-900 bg-zinc-100 rounded-xl border-2 border-solid border-zinc-400 dark:border-zinc-700 hover:border-zinc-600 dark:hover:border-white  transform hover:scale-103 transition-transform">
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -3,15 +3,59 @@
 const Skills = () => {
     return (
         <section id="skills" className="container mx-auto p-5">
-            <h2 className="text-3xl mb-3">Skill</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div>
-                    <h3 className="text-2xl mb-2">Languages and Frameworks</h3>
-                    <p>HTML, CSS, JavaScript, React, Node.js, Express, etc.</p>
+            <h2 className="mb-3 text-start"><span className="text-xl">My Arsenal</span> <br /><span className="font-extrabold text-gray-950 dark:text-gray-100 text-3xl md:text-5xl mb-2 tracking-tight text-start">Skills</span></h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-8">
+                <div className="my-5 shadow-md dark:bg-zinc-900 bg-zinc-100 rounded-xl border-2 border-solid border-zinc-400 dark:border-zinc-700 hover:border-zinc-600 dark:hover:border-white  transform hover:scale-103 transition-transform px-5 pt-5">
+                    <h3 className="text-gray-950 dark:text-gray-100 text-3xl md:text-3xl font-bold mb-8 tracking-tight">Languages and Frameworks</h3>
+                    <div className="grid grid-cols-4 md:grid-cols-5 gap-12 my-4 lg:w-[500px]">
+                        <div>
+                            <img className="hover:scale-110 duration-200 transition cursor-pointer" src="/public/html-1.svg" alt="" />
+                        </div>
+                        <div>
+                            <img className="hover:scale-110 duration-200 transition cursor-pointer" src="/public/css-3.svg" alt="" />
+                        </div>
+                        <div>
+                            <img className="mt-5 hover:scale-110 duration-200 transition cursor-pointer" src="/public/tailwind-css-2 (1).svg" alt="" />
+                        </div>
+                        <div>
+                            <img className="mt-2 hover:scale-110 duration-200 transition cursor-pointer" src="/public/bootstrap-5-1.svg" alt="" />
+                        </div>
+                        <div>
+                            <img className="my-10 lg:my-8 hover:scale-110 duration-200 transition cursor-pointer" src="/public/daisyUI.svg" alt="" />
+                        </div>
+                        <div>
+                            <img className="hover:scale-110 duration-200 transition cursor-pointer" src="/public/logo-javascript.svg" alt="" />
+                        </div>
+                        <div>
+                            <img className="hover:scale-110 duration-200 transition cursor-pointer" src="/public/react-2.svg" alt="" />
+                        </div>
+                        <div>
+                            <img className="hover:scale-110 duration-200 transition cursor-pointer" src="/public/nodejs-icon.svg" alt="" />
+                        </div>
+                        <div>
+                            <img className="lg:w-48 w-40 hover:scale-110 duration-200 transition cursor-pointer" src="https://skillicons.dev/icons?i=express" />
+                        </div>
+                        <div>
+                            <img className="lg:w-48 w-40 hover:scale-110 duration-200 transition cursor-pointer" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg"/>
+                        </div>
+                        <div>
+                            <img src="" alt="" />
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <h3 className="text-2xl mb-2">Database and Tools</h3>
-                    <p>MongoDB, MySQL, Git, etc.</p>
+                <div className="my-5 shadow-md dark:bg-zinc-900 bg-zinc-100 rounded-xl border-2 border-solid border-zinc-400 dark:border-zinc-700 hover:border-zinc-600 dark:hover:border-white  transform hover:scale-103 transition-transform px-5 pt-5">
+                    <h3 className="text-gray-950 dark:text-gray-100 text-3xl md:text-3xl font-bold mb-8 tracking-tight">Database and Tools</h3>
+                    <div className="grid grid-cols-4 md:grid-cols-5 gap-12 my-4">
+                        <div>
+                            <img className="" src="/public/firebase-1.svg" alt="" />
+                        </div>
+                        <div>
+                            <img className="mt-3" src="/public/mongodb-icon-1.svg" alt="" />
+                        </div>
+                        <div>
+                            <img className="mt-3" src="/public/vitejs.svg" alt="" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
