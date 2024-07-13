c30a96005206ea57164e5ac0a9c9b7afda706b44
Sa18-f <sisazid182713@gmail.com>
2024-07-13 13:17:06 +0600

    react countup implemented

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,3 +1,4 @@
+import CountUp from "react-countup";
 import SwiperComponent from "./SwiperComponent";
 
 
@@ -8,11 +9,21 @@ const About = () => {
                 <img className="rounded-xl" src="https://i.ibb.co/hCp3Tp5/Whats-App-Image-2024-07-04-at-15-24-14-9c3fa3ed.jpg" alt="" />
             </div> */}
             <div className="lg:w-1/3 w-[74%]">
-            <SwiperComponent></SwiperComponent>
+                <SwiperComponent></SwiperComponent>
             </div>
             <div className="">
                 {/* <h1 className="text-4xl font-medium text-white"> Hi There! I’m Md. Shoriful Islam</h1> */}
-                <div className="flex gap-7 items-center flex-wrap text-[#0788FF]"><div><span className="blue-text-gradient font-bold lg:text-[44px] text-[30px]"><span>21</span></span><br /><span className="text-gray-400 font-semibold lg:text-normal text-sm">years old</span></div><div><span className="blue-text-gradient font-bold lg:text-[44px] text-[30px]"><span>10</span>+</span><br /><span className="text-gray-400 font-semibold lg:text-normal text-sm">month’s web dev </span></div><div><span className="blue-text-gradient font-bold lg:text-[44px]  text-[30px]"><span>578</span></span><br /><span className="text-gray-400 font-semibold lg:text-normal text-sm">Total Commits</span></div></div>
+                <div className="flex gap-7 items-center flex-wrap text-[#0788FF]">
+                    <div>
+                        <span className="blue-text-gradient font-bold lg:text-[44px] text-[30px]"><span><CountUp end={21}></CountUp></span></span><br /><span className="text-gray-400 font-semibold lg:text-normal text-sm">years old</span>
+                    </div>
+                    <div>
+                        <span className="blue-text-gradient font-bold lg:text-[44px] text-[30px]"><span><CountUp end={10}></CountUp></span>+</span><br /><span className="text-gray-400 font-semibold lg:text-normal text-sm">month’s web dev </span>
+                    </div>
+                    <div>
+                        <span className="blue-text-gradient font-bold lg:text-[44px]  text-[30px]"><span><CountUp end={598}></CountUp></span></span><br /><span className="text-gray-400 font-semibold lg:text-normal text-sm">Total Commits</span>
+                    </div>
+                </div>
                 {/* <h3 className="text-2xl font-medium text-[#0788FF] mt-3">Mern Stack Web Developer</h3> */}
                 <p className="mt-5 text-[18px] max-w-[650px] leading-[30px] text-start text-white" style={{ opacity: 1, transform: 'none' }}>
                     I’m a skilled software developer with experience in <span className="font-bold text-xl">React</span> and <span className="font-bold text-xl">JavaScript</span>, and expertise in frameworks like <span className="font-bold text-xl">Node.Js</span> and <span className="font-bold text-xl">Express.Js</span>. I’m a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let’s work together to bring your ideas to life!
